5ec3deca2af62e7db8a39c71a242437908d8e8d0
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-03 12:12:23 +0530

    Fixed Changes

--- src/Pages/ContactMe/ContactMe.jsx
+++ src/Pages/ContactMe/ContactMe.jsx
@@ -13,18 +13,18 @@ function ContactMe(){
                         <h2 className='left-header-text' id='great-together'>great together.</h2>
                     </div>
                     <div className="email">
-                        {/* <i class="fa-solid fa-envelope"></i> */}
+                        <i class="fa-solid fa-envelope"></i>
                         <h2>abhishek.23bcs10065@ms.sst.scaler.com</h2>
                     </div>
                     <div className='location'>
-                        {/* <i class="fa-solid fa-location-dot"></i> */}
+                        <i class="fa-solid fa-location-dot"></i>
                         <h2>Bangalore, India</h2>
                     </div>
                     <div className="socialIcons">
-                        {/* <a href='https://www.linkedin.com/in/curiousabhishek/'><i class="fa-brands fa-linkedin"></i></a>
+                        <a href='https://www.linkedin.com/in/curiousabhishek/'><i class="fa-brands fa-linkedin"></i></a>
                         <a href='https://github.com/abhishekshah5486'><i class="fa-brands fa-github"></i></a>
                         <a href=''><i class="fa-brands fa-instagram"></i></a>
-                        <a href=''><i class="fa-brands fa-square-x-twitter"></i></a> */}
+                        <a href=''><i class="fa-brands fa-square-x-twitter"></i></a>
                     </div>
                 </div>
                 <div className="contactMe-right-section">
--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -22,11 +22,11 @@ function Skills(){
                 <h2>Skills</h2>
                 <div className="skills-container">
                     <div className="skills-item">
-                        {/* <i class="fa-brands fa-html5"></i> */}
+                        <i class="fa-brands fa-html5"></i>
                         <h2>HTML5</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-css3"></i> */}
+                    <i class="fa-brands fa-css3"></i>
                     <h2>CSS3</h2>
                     </div>
                     <div className="skills-item">
@@ -34,23 +34,23 @@ function Skills(){
                         <h2>JavaScript</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-java"></i> */}
+                    <i class="fa-brands fa-java"></i>
                     <h2>Java</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-python"></i> */}
+                    <i class="fa-brands fa-python"></i>
                     <h2>Python</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-react"></i> */}
+                    <i class="fa-brands fa-react"></i>
                     <h2>ReactJs</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-git-alt"></i> */}
+                    <i class="fa-brands fa-git-alt"></i>
                     <h2>Git</h2>
                     </div>
                     <div className="skills-item">
-                    {/* <i class="fa-brands fa-github"></i> */}
+                    <i class="fa-brands fa-github"></i>
                     <h2>Github</h2>
                     </div>
                     <div className="skills-item">
